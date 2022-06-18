263386a86442cb693bf8b7d8a04b9ab47d884b86
Regina <b.reggi.v@gmail.com>
2022-06-18 14:16:29 +0300

    Первое дз выполнено

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument(value) {}
+function returnFirstArgument(value) {
+  return value;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,9 @@ function returnFirstArgument(value) {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +42,10 @@ function sumWithDefaults(a, b) {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+
+function returnFnResult(fn) {
+  return fn();
+}
 
 /*
  Задание 4:
@@ -56,7 +63,12 @@ function returnFnResult(fn) {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+
+function returnCounter(n = 0) {
+  return function f() {
+    return ++n;
+  };
+}
 
 /*
  Задание 5 *:
@@ -67,7 +79,15 @@ function returnCounter(number) {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray() {
+  const array = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    array[i] = arguments[i];
+  }
+
+  return array;
+}
 
 /*
  Задание 6 *:
@@ -84,7 +104,11 @@ function returnArgumentsArray() {}
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn, ...args) {}
+function bindFunction(fn, ...args) {
+  return function newFunction() {
+    return fn(...args);
+  };
+}
 
 export {
   returnFirstArgument,
